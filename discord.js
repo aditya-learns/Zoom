7b208a65f74f1f118cd0704539bcfc31cc78259d
Aditya Gupta <adityagupta3011@gmail.com>
2021-01-27 20:24:25 +0530

    first commit

---
+++ discord.js
@@ -0,0 +1,22 @@
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const WOKCommands = require('wokcommands')
+const Perspective = require('perspective-api-client');
+const perspective = new Perspective({apiKey:"AIzaSyCDnKgR033P5zJscXW9AsACiwqE2rdHyts"});
+require('dotenv').config();
+const {Toxic, Sexuality, Insult, spam} = require('./config.json')
+
+client.on('ready', () => {
+  console.log('I am ready!');
+  new WOKCommands(client, {
+    commandsDir: 'commands',
+    messagesPath,
+    showWarns: true
+  })
+});
+
+client.on('message', async (message) => {
+})
+
+client.login(process.env.DISCORD_TOKEN);
---
+++ perspective.js
@@ -0,0 +1,22 @@
+const Perspective = require('perspective-api-client');
+const perspective = new Perspective({apiKey:"AIzaSyCDnKgR033P5zJscXW9AsACiwqE2rdHyts"});
+async function eval()
+{
+    let result = await perspective.analyze(message.content,{attributes: ['toxicity','spam','insult','Sexuality']} );
+  if(result.attributeScores.TOXICITY.summaryScore.value > Toxic)
+  {
+    message.channel.send(`Please don't send toxic messages`)
+  }
+  else if(result.attributeScores.SPAM.summaryScore.value > spam)
+  {
+    message.reply(`Please don't spam`);
+  }
+  else if(result.attributeScores.INSULT.summaryScore.value > Insult)
+  {
+    message.reply(`Please don't insult anyone`);
+  }
+  else if(result.attributeScores.SEXUALITY.summaryScore.value > Sexuality)
+  {
+    message.reply(`Please don't spam`);
+  }
+}
